cf7e3912c66a51198a9d61b94af9c0bcfc9c59f6
Anthony Gargiulo <anthony@agargiulo.com>
2012-05-10 03:07:43 -0400

    Initial Commit

---
+++ src/com/agargiulo/code/bukkit/respawn_item/RespawnItem.java
@@ -0,0 +1,25 @@
+/**
+ * File: RespawnItem.java
+ * Date created: May 10, 2012
+ * @author Anthony Gargiulo
+ */
+package com.agargiulo.code.bukkit.respawn_item;
+
+
+/**
+ * @author Anthony Gargiulo
+ *
+ */
+public class RespawnItem
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
