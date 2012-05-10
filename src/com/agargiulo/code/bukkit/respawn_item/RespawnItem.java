3f99473fc60dccf4738b0e16c37c59a8b72a3fcf
Anthony Gargiulo <anthony@agargiulo.com>
2012-05-10 04:06:23 -0400

    Plugin is ready for some testing. as of now, it /should/ work with minimal features

--- src/com/agargiulo/code/bukkit/respawn_item/RespawnItem.java
+++ src/com/agargiulo/code/bukkit/respawn_item/RespawnItem.java
@@ -1,16 +1,25 @@
 /**
  * File: RespawnItem.java
  * Date created: May 10, 2012
+ * 
  * @author Anthony Gargiulo
  */
+
 package com.agargiulo.code.bukkit.respawn_item;
 
+import java.util.logging.Logger;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author Anthony Gargiulo
- *
+ * 
  */
-public class RespawnItem
+public class RespawnItem extends JavaPlugin
 {
 
 	/**
@@ -20,6 +29,36 @@ public class RespawnItem
 	{
 		// TODO Auto-generated method stub
 
+	}
+
+	private Logger	log;
+
+	@Override
+	public void onDisable()
+	{
+		log = getLogger();
+		log.info("Your plugin has been disabled");
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+		log = getLogger();
+		log.info("Your plugin has been enabled.");
+	}
+
+	/**
+	 * @param event
+	 * 
+	 */
+	public void onPlayerRespawn(PlayerRespawnEvent event)
+	{
+		Player player = event.getPlayer(); // The player who respawned
+		PlayerInventory playerInv = player.getInventory(); // Their items
+		ItemStack items = new ItemStack(371);
+		playerInv.addItem(items);
+		player.sendMessage("You just respawned, have some gold.");
 	}
 
 }
