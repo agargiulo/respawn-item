614911ca260c90443556ae2de0fcc3f00cc05027
Anthony Gargiulo <anthony@agargiulo.com>
2012-05-11 03:37:37 -0400

    The plugin actually works now, so bumped to version 0.7 from 0.1 Turns out I was not actually listening for events at all. Thanks to the folks from #bukkitdev. This commit closes issue #1

--- src/com/agargiulo/code/bukkit/respawn_item/RespawnItem.java
+++ src/com/agargiulo/code/bukkit/respawn_item/RespawnItem.java
@@ -1,76 +1,36 @@
-/**
- * File: RespawnItem.java
- * Date created: May 10, 2012
- * 
- */
 
 package com.agargiulo.code.bukkit.respawn_item;
 
-import java.util.logging.Logger;
-
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author Anthony Gargiulo (anthony@agargiulo.com)
  * 
  */
-public class RespawnItem extends JavaPlugin
+public class RespawnItem extends JavaPlugin implements Listener
 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	private Logger	log;
-
-	@Override
-	public void onDisable()
-	{
-		log = getLogger();
-		log.info("Your plugin has been disabled");
-
-	}
 
 	@Override
 	public void onEnable()
 	{
-		log = getLogger();
-		log.info("Your plugin has been enabled.");
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	/**
-	 * @param event
-	 */
-	public void onPlayerJoin(PlayerJoinEvent event)
-	{
-		log.info("Player " + event.getPlayer().getDisplayName()
-				+ " has joined the game");
-	}
-
-	/**
-	 * @param event
+	 * Give every player that respawns a single gold nugget.
 	 * 
+	 * @param event
 	 */
+	@EventHandler
 	public void onPlayerRespawn(PlayerRespawnEvent event)
 	{
-		Player player = event.getPlayer(); // The player who respawned
-		PlayerInventory playerInv = player.getInventory(); // Their items
-		ItemStack items = new ItemStack(Material.GOLD_NUGGET);
-		playerInv.addItem(items);
-		player.getInventory().setContents(playerInv.getContents());
-		player.sendMessage("You just respawned, have some gold.");
-		log.info("Player respawned");
+		event.getPlayer().getInventory()
+				.addItem(new ItemStack(Material.GOLD_NUGGET));
 	}
 
 }
